2602d0fd415e3d421ae3a480a85e72f671458cc3
SundarDong <brosisstudio123@gmail.com>
2024-11-15 13:20:03 +0545

    Silver app bar and tab bar working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_current_location.dart';
+import 'package:food_delivery_app/components/my_description.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
+import 'package:food_delivery_app/components/my_tab_bar.dart';
+import 'package:food_delivery_app/pages/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,16 +12,69 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  //tab controller
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Homepage"),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        ),
+
         drawer: const MyDrawer(),
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled)=> [
+            MySliverAppBar(
+              title:MyTabBar(
+                tabController: _tabController ,
+                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Divider(
+                    indent: 25,
+                    endIndent: 25,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+
+                  //current location 
+                 const MyCurrentLocation(),
+
+                  //description box
+                 const MyDescriptionBox(),
+                ],
+              ),
+               ),
+          ],
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+                ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context,index)=> Text("first tab items"),
+                ),
+                ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context,index)=> Text("Second tab items"),
+                ),
+                ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context,index)=> Text("Third tab items"),
+                ),
+            ],
+           ),
+        ),
     );
   }
 }
